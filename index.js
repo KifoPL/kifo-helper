93475e3f3ed47967583e6524aea63893240eb3c3
KifoPL <kifounaabi@gmail.com>
2021-08-06 16:29:24 +0200

    updated packages

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-require('dotenv').config()
-const Discord = require('discord.js');
-const fs = require('fs');
-const kifo = require('kifo');
-const config = require('./configuration.json')
-const main = require(`./index.js`)
-const mysql = require("mysql")
+require("dotenv").config();
+const Discord = require("discord.js");
+const fs = require("fs");
+const kifo = require("kifo");
+const config = require("./configuration.json");
+const main = require(`./index.js`);
+const mysql = require("mysql");
 
-const client = new Discord.Client();
+//client login
+const client = new Discord.Client({
+	partials: [`MESSAGE`, `CHANNEL`, `REACTION`],
+	intents: [
+		Discord.Intents.FLAGS.GUILDS,
+		Discord.Intents.FLAGS.GUILD_MEMBERS,
+		Discord.Intents.FLAGS.GUILD_INTEGRATIONS,
+		Discord.Intents.FLAGS.GUILD_WEBHOOKS,
+		Discord.Intents.FLAGS.GUILD_INVITES,
+		Discord.Intents.FLAGS.GUILD_PRESENCES,
+		Discord.Intents.FLAGS.GUILD_MESSAGES,
+		Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+		Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING,
+		Discord.Intents.FLAGS.DIRECT_MESSAGES,
+		Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+		Discord.Intents.FLAGS.DIRECT_MESSAGE_TYPING,
+	],
+});
 client.commands = new Discord.Collection();
 var con;
 var KifoClanker = {};
@@ -17,7 +34,7 @@ var dbconfig = {
 	host: process.env.HOST,
 	user: process.env.USER,
 	password: process.env.PASSWORD,
-	database: "kifo_helper",
+	database: "kifo_helper_db",
 };
 
 //Database connection
@@ -37,7 +54,7 @@ function dbReconnect() {
 		if (err.code === "PROTOCOL_CONNECTION_LOST") {
 			dbReconnect();
 		} else {
-			main.log(err, "BOT IS LIKELY SHUT DOWN")
+			main.log(err, "BOT IS LIKELY SHUT DOWN");
 			throw err;
 		}
 	});
@@ -50,7 +67,9 @@ module.exports.SupportGuild = SupportGuild;
 module.exports.maintenanceEnd = maintenanceEnd;
 
 // Commands loader
-const commandFiles = fs.readdirSync(`./commands`).filter(file => file.endsWith(".js"))
+const commandFiles = fs
+	.readdirSync(`./commands`)
+	.filter((file) => file.endsWith(".js"));
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.name, command);
@@ -58,53 +77,74 @@ for (const file of commandFiles) {
 }
 
 // On Ready
-client.once('ready', async () => {
-	console.log('Ready!');
+client.once("ready", async () => {
+	console.log("Ready!");
 	SupportGuild = await client.guilds.fetch("822800862581751848");
 	KifoClanker = await client.users.fetch("795638549730295820");
 
 	checkKifoClanker();
-	setInterval(checkKifoClanker, 1000 * 2)
+	setInterval(checkKifoClanker, 1000 * 2);
 });
 
-client.on("message", (message) => {
+client.on("error", (err) => console.log(err));
 
+client.on("message", (message) => {
 	if (message.author.id !== "289119054130839552") return;
 	if (!message.content.startsWith(config.prefix)) return;
 	let args = message.content.slice(config.prefix.length).split(" ");
 	let cmd = args.shift().toLowerCase();
 	if (client.commands.has(cmd)) {
-		client.commands.get(cmd).execute(message, args)
+		client.commands.get(cmd).execute(message, args);
 	}
-})
+});
 
 async function checkKifoClanker() {
-	KifoClanker = await client.users.fetch("795638549730295820", { force: true })
+	KifoClanker = await client.users.fetch("795638549730295820", {
+		force: true,
+	});
 
 	let now = new Date(Date.now());
 
 	if (KifoClanker.presence.status == "online" && !isKifoOnline) {
 		isKifoOnline = true;
-		let alertChannel = SupportGuild.channels.resolve("867682507592171551")
-		alertChannel.send(`<@&867682973939138590>`, kifo.embed("<@!795638549730295820> has recovered from unexpected crash! 🥳", `Unplanned downtime alert!`)).then(msg => {
-			msg.crosspost().catch(err => main.log(err))
-		}).catch(err => main.log(err))
+		let alertChannel = SupportGuild.channels.resolve("867682507592171551");
+		alertChannel
+			.send(
+				`<@&867682973939138590>`,
+				kifo.embed(
+					"<@!795638549730295820> has recovered from unexpected crash! 🥳",
+					`Unplanned downtime alert!`
+				)
+			)
+			.then((msg) => {
+				msg.crosspost().catch((err) => main.log(err));
+			})
+			.catch((err) => main.log(err));
 	}
 
 	if (KifoClanker.presence.status == "offline" && isKifoOnline) {
-
-		let Kifo = await client.users.fetch("289119054130839552", { force: true })
-		console.log(`KifoClanker is offline!!! ${now.toUTCString()}`)
+		let Kifo = await client.users.fetch("289119054130839552", {
+			force: true,
+		});
+		console.log(`KifoClanker is offline!!! ${now.toUTCString()}`);
 		isKifoOnline = false;
-		let alertChannel = SupportGuild.channels.resolve("867682507592171551")
-		let alertEmbed = kifo.embed(`<@!795638549730295820> had an unexpected crash. <@!289119054130839552> has been notified, he ${Kifo.presence.status == "offline" ? "is currently offline, so it may take some time until he fixes the bot." : "will fix the bot soon"}.`, "Unplanned downtime alert!")
-		alertChannel.send(`<@&867682973939138590>`, alertEmbed).then(msg => {
-			msg.crosspost().catch(err => main.log(err))
-		}).catch(err => main.log(err))
+		let alertChannel = SupportGuild.channels.resolve("867682507592171551");
+		let alertEmbed = kifo.embed(
+			`<@!795638549730295820> had an unexpected crash. <@!289119054130839552> has been notified, he ${
+				Kifo.presence.status == "offline"
+					? "is currently offline, so it may take some time until he fixes the bot."
+					: "will fix the bot soon"
+			}.`,
+			"Unplanned downtime alert!"
+		);
+		alertChannel
+			.send(`<@&867682973939138590>`, alertEmbed)
+			.then((msg) => {
+				msg.crosspost().catch((err) => main.log(err));
+			})
+			.catch((err) => main.log(err));
 	}
 }
-
-
 
 client.login(process.env.LOGIN_TOKEN);
 
@@ -114,6 +154,7 @@ client.login(process.env.LOGIN_TOKEN);
  * @returns Promise, in case something breaks
  */
 exports.log = function (log, ...args) {
+	console.log(log);
 	let channel = client.guilds
 		.resolve("822800862581751848")
 		.channels?.resolve("864112365896466432");
@@ -141,7 +182,7 @@ exports.log = function (log, ...args) {
 		});
 };
 
-process.on('uncaughtException', async (err) => {
-	console.error(err)
-	await main.log(err)
-})
+// process.on('uncaughtException', async (err) => {
+// 	console.error(err)
+// 	await main.log(err)
+// })
